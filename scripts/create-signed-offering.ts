5cc509de1db176fbee81650edf7ea303ae778ec3
Simon Petrac <pfoerdie@gmail.com>
2024-08-14 13:50:24 +0200

    reduced code and added file for credentials context

--- scripts/create-signed-offering.ts
+++ scripts/create-signed-offering.ts
@@ -1,63 +1,56 @@
 import * as vc from '@digitalbazaar/vc'
 import { Ed25519VerificationKey2020 } from '@digitalbazaar/ed25519-verification-key-2020'
 import { Ed25519Signature2020 } from '@digitalbazaar/ed25519-signature-2020'
-import * as securityContexts from '@digitalbazaar/security-context'
 
-type DocumentLoader = (url: string) => Promise<{
-  contextUrl?: string | null,
-  documentUrl: string,
-  document: object,
-  tag?: string
-}>
+type ContextDocument = { '@context': Record<string, any> }
 
-const contexts = new Map([
-  [securityContexts.constants.SECURITY_CONTEXT_V1_URL, securityContexts.contexts.get(securityContexts.constants.SECURITY_CONTEXT_V1_URL)],
-  [securityContexts.constants.SECURITY_CONTEXT_V2_URL, securityContexts.contexts.get(securityContexts.constants.SECURITY_CONTEXT_V2_URL)]
-])
+const { constants: { SECURITY_CONTEXT_V1_URL, SECURITY_CONTEXT_V2_URL }, contexts: securityContext } = await import('@digitalbazaar/security-context')
+const contexts: Record<string, ContextDocument> = Object.create(null)
+contexts[SECURITY_CONTEXT_V1_URL] = securityContext.get(SECURITY_CONTEXT_V1_URL)
+contexts[SECURITY_CONTEXT_V2_URL] = securityContext.get(SECURITY_CONTEXT_V2_URL)
+const DOME_CONTEXT_V1_URL = 'https://dome-marketplace.eu/credentials/v1'
+contexts[DOME_CONTEXT_V1_URL] = (await import('../model/credentials.json')).default
 
-function extendContextLoader(documentLoader: DocumentLoader): DocumentLoader {
-  return async function (url) {
-    const context = contexts.get(url);
-    if (context !== undefined) return {
-      contextUrl: null,
-      documentUrl: url,
-      document: context,
-      tag: 'static'
+async function documentLoader(url: string): Promise<{ documentUrl: string, document: ContextDocument }> {
+  if (!contexts[url]) {
+    try {
+      const response = await fetch(url)
+      const document = await response.json() as ContextDocument
+      if (!document['@context']) throw new Error('invalid ContextDocument')
+      contexts[url] = document
+    } catch (err) {
+      console.error(err)
+      return vc.defaultDocumentLoader(url)
     }
-    return documentLoader(url);
+  }
+
+  return {
+    documentUrl: url,
+    document: contexts[url]
   }
 }
 
-const documentLoader = extendContextLoader(async function (url) {
-  if (url === 'did:test:context:foo') {
-    return {
-      contextUrl: null,
-      documentUrl: url,
-      document: {
-        // myCustomContext
-      }
-    }
-  }
-  return vc.defaultDocumentLoader(url)
+const suite = new Ed25519Signature2020({
+  key: await Ed25519VerificationKey2020.generate()
 })
 
-const keyPair = await Ed25519VerificationKey2020.generate()
-const suite = new Ed25519Signature2020({ key: keyPair })
+const offering = (await import('../examples/product-offering-elliot-smart-city.json')).default
 
 const credential = {
   "@context": [
     "https://www.w3.org/2018/credentials/v1",
-    "https://www.w3.org/2018/credentials/examples/v1"
+    // "https://www.w3.org/ns/credentials/v2",
+    DOME_CONTEXT_V1_URL
   ],
-  "id": "https://example.com/credentials/1872",
-  "type": ["VerifiableCredential", "AlumniCredential"],
-  "issuer": "https://example.edu/issuers/565049",
-  "issuanceDate": "2010-01-01T19:23:24Z",
-  "credentialSubject": {
-    "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-    "alumniOf": "Example University"
-  }
+  "id": "https://example.com/credentials/" + offering.id,
+  "type": ["VerifiableCredential"],
+  "issuer": offering.productSpecification.relatedParty[0].href,
+  "issuanceDate": new Date().toISOString(),
+  "credentialSubject": offering
 }
+
+//@ts-ignore // NOTE quick fix for suite error
+suite.verificationMethod = () => true
 
 const signedVC = await vc.issue({ credential, suite, documentLoader })
 console.log(JSON.stringify(signedVC, null, 2))
