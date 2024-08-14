9ace5b5f2b49a57eaf505d9793714101300f3992
Simon Petrac <pfoerdie@gmail.com>
2024-08-14 11:42:25 +0200

    wip: trying to get signed document

--- scripts/create-signed-offering.ts
+++ scripts/create-signed-offering.ts
@@ -1,8 +1,45 @@
-// TODO
-
-import vc from '@digitalbazaar/vc'
-import { Ed25519Signature2020 } from '@digitalbazaar/ed25519-signature-2020'
+import * as vc from '@digitalbazaar/vc'
 import { Ed25519VerificationKey2020 } from '@digitalbazaar/ed25519-verification-key-2020'
+import { Ed25519Signature2020 } from '@digitalbazaar/ed25519-signature-2020'
+import * as securityContexts from '@digitalbazaar/security-context'
+
+type DocumentLoader = (url: string) => Promise<{
+  contextUrl?: string | null,
+  documentUrl: string,
+  document: object,
+  tag?: string
+}>
+
+const contexts = new Map([
+  [securityContexts.constants.SECURITY_CONTEXT_V1_URL, securityContexts.contexts.get(securityContexts.constants.SECURITY_CONTEXT_V1_URL)],
+  [securityContexts.constants.SECURITY_CONTEXT_V2_URL, securityContexts.contexts.get(securityContexts.constants.SECURITY_CONTEXT_V2_URL)]
+])
+
+function extendContextLoader(documentLoader: DocumentLoader): DocumentLoader {
+  return async function (url) {
+    const context = contexts.get(url);
+    if (context !== undefined) return {
+      contextUrl: null,
+      documentUrl: url,
+      document: context,
+      tag: 'static'
+    }
+    return documentLoader(url);
+  }
+}
+
+const documentLoader = extendContextLoader(async function (url) {
+  if (url === 'did:test:context:foo') {
+    return {
+      contextUrl: null,
+      documentUrl: url,
+      document: {
+        // myCustomContext
+      }
+    }
+  }
+  return vc.defaultDocumentLoader(url)
+})
 
 const keyPair = await Ed25519VerificationKey2020.generate()
 const suite = new Ed25519Signature2020({ key: keyPair })
@@ -22,5 +59,5 @@ const credential = {
   }
 }
 
-const signedVC = await vc.issue({ credential, suite })
+const signedVC = await vc.issue({ credential, suite, documentLoader })
 console.log(JSON.stringify(signedVC, null, 2))
